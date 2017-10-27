6e9c9d7aea3edecef5d7ecc5a2a3000bb2bdcef7
Jonathan Kirst <jskirst@gmail.com>
2017-10-27 10:14:57 -0700

    Switching demos was too hard

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -1,7 +1,23 @@
 class DemosController < ApplicationController
+  before_action :load_server
+  before_action :load_demo
 
-  def basic
-
+  def show
   end
 
+  private
+
+  def load_server
+    @server ||= begin
+      if params[:server_id] == "prod"
+        "edward-api"
+      else
+        "edward-api-staging"
+      end
+    end
+  end
+
+  def load_demo
+    @demo = params[:id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #
-  root to: 'demos#basic'
+  resources :servers, only: [] do
+    resources :demos, only: [:show]
+  end
 end
