072e6d33e6fc818f8ebc9c9a80099a4909cfea8f
Jonathan Kirst <jskirst@gmail.com>
2017-12-07 14:00:07 -0800

    Polyfilling

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -1,12 +1,19 @@
 Vue.use(Vuetify)
 Vue.use(EdwardsVoice);
 
+Vue.config.errorHandler = function (err, vm, info) {
+  console.log(err);
+}
+
 new Vue({
   el: '#basic-app',
   template: '#basic-template',
   methods: {
     emitNext: function() {
-      this.$refs.steplist.stepForward();
+      console.log(this.validClient);
+      if(this.validClient){
+        this.$refs.steplist.stepForward();
+      }
     }
   }
 })
